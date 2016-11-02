651d0d5d1764d2e8abb8803925fc91ecaf93f1c8
Matthew P. Slight <matthew@slight.me>
2016-11-02 19:47:38 +0400

    updated test file

--- tests/all_tests.py
+++ tests/all_tests.py
@@ -8,8 +8,10 @@ class MyTest(LiveServerTestCase):
   def create_app(self):
     app = Flask(__name__)
     app.config['TESTING'] = True
+    app.config['LIVESERVER_PORT'] = 8943
     return app
 
   def test_flask_application_is_up_and_running(self):
+    print(self.get_server_url())
     response = urlopen(self.get_server_url())
     self.assertEqual(response.code, 200) 
