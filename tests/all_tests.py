f1e9ae960331cf8dc382fb734bf77c0d425d2f8e
Matthew P. Slight <matthew@slight.me>
2016-11-02 13:16:36 +0400

    modified test

---
+++ tests/all_tests.py
@@ -0,0 +1,20 @@
+import json
+from urllib.request import urlopen
+from flask import Flask
+from flask_testing import LiveServerTestCase
+
+class MyTest(LiveServerTestCase):
+
+  def create_app(self):
+    app = Flask(__name__)
+    app.config['TESTING'] = True
+    return app
+
+  def test_status(self):
+    expected = {
+      "status": "Status 0"
+    }
+
+  def test_flask_application_is_up_and_running(self):
+    response = urlopen(self.get_server_url())
+    self.assertEqual(response.code, 200) 
